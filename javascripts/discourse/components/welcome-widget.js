1567db358c8942e31dfc7fffb634e53816af45bb
JohnSmithW <salapin.danil1@mail.ru>
2023-09-01 14:57:39 +0300

    Fixed some issues

--- javascripts/discourse/components/welcome-widget.js
+++ javascripts/discourse/components/welcome-widget.js
@@ -1,14 +1,14 @@
-import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
-import { defaultHomepage } from "discourse/lib/utilities";
+// import Component from "@ember/component";
+// import discourseComputed from "discourse-common/utils/decorators";
+// import { inject as service } from "@ember/service";
+// import { defaultHomepage } from "discourse/lib/utilities";
 
-export default Component.extend({
-  tagName: "",
-  router: service(),
+// export default Component.extend({
+//   tagName: "",
+//   router: service(),
 
-  @discourseComputed("router.currentRouteName")
-  shouldDisplay(currentRouteName) {
-    return currentRouteName === `discovery.${defaultHomepage()}`;
-  },
-});
+//   @discourseComputed("router.currentRouteName")
+//   shouldDisplay(currentRouteName) {
+//     return currentRouteName === `discovery.${defaultHomepage()}`;
+//   },
+// });
